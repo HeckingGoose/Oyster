442ded7af6071e72c31a70c5adbb1d0ef4424534
HeckingGoose <a.bulman6453@gmail.com>
2025-12-17 18:26:41 +0000

    Line marker command implemented.

---
+++ Oyster/Commands/Line_Marker.cs
@@ -0,0 +1,36 @@
+﻿using Oyster.Core.AbstractTypes.Commands;
+using Oyster.Core.Interfaces.Commands;
+
+namespace Oyster.Commands
+{
+    public class Line_Marker : A_Command, ILineMarker
+    {
+        // Private Variables
+        private string _name;
+
+        // Constructor
+        public Line_Marker(string name)
+        {
+            // Pass Value
+            _name = name;
+        }
+
+        // Public Methods
+        public static ISpeechCommand? MakeSelf(string[] rawParameters)
+        {
+            // Read in first param as string
+            string? name = null;
+            bool success = LoadParameterValue(rawParameters[0], ref name);
+
+            // Given failure return null
+            if (!success || name == null) return null;
+
+            // Otherwise return new self
+            return new Line_Marker(name);
+        }
+        public override bool Run() { return true; }
+
+        // Accessors
+        public string Name { get { return _name; } }
+    }
+}
--- Oyster/Core/Interfaces/Commands/ILineMarker.cs
+++ Oyster/Core/Interfaces/Commands/ILineMarker.cs
@@ -3,6 +3,9 @@ namespace Oyster.Core.Interfaces.Commands
     public interface ILineMarker
     {
         // Accessors
+        /// <summary>
+        /// The name of this line marker.
+        /// </summary>
         public string Name { get; }
     }
 }
